c25897854a06c4df3679cc5fa86c355bf020255c
bicknoland-netizen <bicknoland@gmail.com>
2025-11-23 19:23:28 -0330

    Added the db.py for the read and write money files, also added the money.txt file

---
+++ db.py
@@ -0,0 +1,20 @@
+"""Store the functions for writing and reading 
+the money amount to a file any time the data 
+is changed."""
+
+import sys
+
+def read_money():
+    try:
+        with open("money.txt", 'r') as file:
+                contents = file.read()
+                print(contents)
+    except FileNotFoundError:
+        print("Error: money.txt has not been found")
+        print("Tarminating program.")
+        sys.exit()
+
+
+def write_money(amount):
+        with open("money.txt", 'w') as file:
+                file.write(f"{amount}")
