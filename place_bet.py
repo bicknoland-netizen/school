35f174de8a13366bd12fbf2ec3a93ea681e56639
bicknoland-netizen <bicknoland@gmail.com>
2025-11-23 18:32:40 -0330

    Finished the betting module

--- place_bet.py
+++ place_bet.py
@@ -1,16 +1,48 @@
 """A module to place a bet and ensure
 proper error handling"""
 
-#Todo: Must add ability for player to purchase more money.
+import db
+import sys
 
-def place_bet(money):
+#Todo: Must add ability for player to purchase more money.
+#Add a check of players money during this function.
+
+def place_bet():
+
     while True:
-        print(f"Money: {money}")
+        remainder = db.read_money()
+        if float(remainder) < 5:
+            add_funds(remainder)
+            continue
         try:
+            
+            print(f"Money: {remainder}")
             bet_amount = float(input("Bet Amount: "))
             if bet_amount < 5:
                 print("Error: Lowest bet is 5")
                 continue
-            return bet_amount
+            elif bet_amount > 1000:
+                print("Error: Maximum bet is 1000")
+                continue
+            elif bet_amount > remainder:
+                print("Error: You don't have that many chips")
+                continue
+
+            remainder -= bet_amount
+            db.write_money(remainder)
+            break
         except ValueError:
             print("Error: Bet must be a float value.")
+            
+
+"""A functio to allow player to purchase more chips or quit playing."""
+        
+def add_funds(remainder):
+    print(f"Your current bank is {remainder}, which is less than the minimal bet of 5")
+    get_more_chips = input("Would you like to purchase more chips? (y/n)?: ")
+    if get_more_chips.lower() == "y":
+        remainder += 100
+        db.write_money(remainder)
+    else:
+        print("Not enough chips to continue. Terminating program.")
+        sys.exit()
